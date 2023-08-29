a63fa6b49138ac6fdb876093ee9a2be26433c222
blakeNaccarato <blake.naccarato@gmail.com>
2023-08-29 13:16:20 -0700

    Migrate more common tests over

--- src/boilercore/testing.py
+++ src/boilercore/testing.py
@@ -1,67 +1,46 @@
 """Helper functions for testing boiler code."""
 
-from collections.abc import Iterable, Iterator
+from collections.abc import Iterable
 from contextlib import contextmanager
 from pathlib import Path
 from re import compile
 from shutil import copy, copytree
 
 import pytest
-from nbqa.__main__ import _get_nb_to_tmp_mapping, _save_code_sources  # type: ignore
+from ploomber_engine.ipython import PloomberClient
 
 
-def change_workdir_and_prepend(tmp_path: Path, monkeypatch: pytest.MonkeyPatch) -> Path:
-    """Change directory and prepend test directory root to `sys.path`.
+def get_nb_client(request, tmp_path, monkeypatch):
+    """Prepare a temporary working directory and return a notebook client."""
+    with before_tmp_workdir(tmp_path, monkeypatch):
+        nb = tmp_path / request.param.name
+        copy(request.param, nb)
+    return PloomberClient.from_path(tmp_path / request.param.name)
 
-    Returns the original working directory."""
-    orig = Path.cwd()
-    monkeypatch.chdir(tmp_path)
-    monkeypatch.syspath_prepend(tmp_path)
-    return orig
+
+def tmp_workdir(tmp_path: Path, monkeypatch: pytest.MonkeyPatch):
+    """Prepare a temporary working directory."""
+    with before_tmp_workdir(tmp_path, monkeypatch):
+        ...
 
 
 @contextmanager
-def get_tmp_project(tmp_path: Path, monkeypatch: pytest.MonkeyPatch) -> Iterator[Path]:
+def before_tmp_workdir(tmp_path: Path, monkeypatch: pytest.MonkeyPatch):
+    """Copy files over, allow changes, then change the working directory."""
     try:
-        data = Path("data")
-        test_data = Path("tests") / data
-        copytree(test_data, tmp_path / data, dirs_exist_ok=True)
-        params = "params.yaml"
-        copy(test_data / params, tmp_path / params)
-        yield test_data
+        copytree(Path("tests") / "tmp_path", tmp_path, dirs_exist_ok=True)
+        yield
     finally:
-        change_workdir_and_prepend(tmp_path, monkeypatch)
+        monkeypatch.chdir(tmp_path)
 
 
-def make_tmp_nbs_content(nbs: list[Path], tmp_path: Path):
-    """Copy notebook contents to importable scripts in the test directory root."""
-    for nb in nbs:
-        (tmp_path / nb.with_suffix(".py").name).write_text(
-            encoding="utf-8", data=get_nb_content(nb)
-        )
+def walk_modules(package: Path, top: Path, suffix: str = ".py") -> Iterable[str]:
+    """Walk modules from a given submodule path and the top level library directory."""
+    for module in walk_module_paths(package, top, suffix):
+        yield get_module(module, top)
 
 
-def get_nb_content(nb: Path) -> str:
-    """Get the contents of a notebook."""
-    _, (newlinesbefore, _) = _save_code_sources(
-        _get_nb_to_tmp_mapping(
-            [str(nb)],
-            None,
-            None,
-            False,
-        ),
-        [],
-        [],
-        False,
-        "abc",
-    )
-    script = next(Path(script) for script in list(newlinesbefore.keys()))
-    contents = script.read_text(encoding="utf-8")
-    script.unlink()
-    return contents
-
-
-def walk_modules(package: Path, top: Path) -> Iterable[str]:
+def walk_module_paths(package: Path, top: Path, suffix: str = ".py") -> Iterable[Path]:
     """Walk modules from a given submodule path and the top level library directory."""
     for directory in (
         package,
@@ -71,8 +50,7 @@ def walk_modules(package: Path, top: Path) -> Iterable[str]:
             if path.is_dir() and "__" not in str(path.relative_to(top.parent))
         ],
     ):
-        for example in sorted(directory.glob("[!__]*.py")):
-            yield (get_module(example, top))
+        yield from sorted(directory.glob(f"[!__]*{suffix}"))
 
 
 def get_module(submodule: Path, library: Path) -> str:
