6d7ebc596f94cd31c33c30c3fd3db87e85bcfab6
blakeNaccarato <blake.naccarato@gmail.com>
2023-11-11 18:31:05 -0800

    Implement notebook namespace caching and results trimming

--- src/boilercore/testing.py
+++ src/boilercore/testing.py
@@ -5,12 +5,21 @@ from inspect import getmembers, isclass, isfunction
 from pathlib import Path
 from shutil import copytree
 from types import ModuleType, SimpleNamespace
-from typing import Any, NamedTuple, TypeAlias
+from typing import Any, Literal, NamedTuple, TypeAlias
 
 import pytest
+from cachier import cachier
+from cachier.core import _default_hash_func
+from nbformat import NO_CONVERT, reads
 from numpy.typing import ArrayLike
 from ploomber_engine._util import parametrize_notebook
 from ploomber_engine.ipython import PloomberClient
+
+
+class MFParam(NamedTuple):
+    run: str
+    y: ArrayLike
+    expected: dict[str, float]
 
 
 def get_session_path(
@@ -25,91 +34,137 @@ def get_session_path(
     return session_path
 
 
-NO_PARAMETERS = {}
+Fun: TypeAlias = Callable[[SimpleNamespace], Any]
+Parameters: TypeAlias = dict[str, Any]
+Results: TypeAlias = list[str]
+Marks: TypeAlias = list[pytest.Mark]
+NotebookCase: TypeAlias = tuple[Path, dict[str, Any], Fun]
 
-
-def get_nb_namespace(
-    nb_client: PloomberClient, parameters: dict[str, Any] = NO_PARAMETERS
-) -> SimpleNamespace:
-    """Copy a notebook and get its namespace."""
-    if parameters:
-        parametrize_notebook(nb_client._nb, parameters=parameters)  # noqa: SLF001
-    return SimpleNamespace(**nb_client.get_namespace())
-
-
-def get_nb_client(nb: Path) -> PloomberClient:
-    """Copy a notebook and get its client."""
-    return PloomberClient.from_path(nb)
-
-
-class MFParam(NamedTuple):
-    run: str
-    y: ArrayLike
-    expected: dict[str, float]
-
-
-NotebookCase: TypeAlias = tuple[Path, dict[str, Any], Callable[[SimpleNamespace], Any]]
-
-
-def get_notebook_cases(notebook: Path, cases: ModuleType) -> Iterator[NotebookCase]:
-    """Get cases."""
-    yield from (  # type: ignore
-        pytest.param((notebook, parameters, fun), marks=marks, id=id)
-        for fun, parameters, marks, id in walk_module_cases(cases)  # noqa: A001
-    )
+NO_PARAMS = {}
+NO_RESULTS = []
+NO_MARKS = []
 
 
 class Case(NamedTuple):
-    fun: Callable[[SimpleNamespace], Any]
-    parameters: dict[str, Any]
-    marks: list[pytest.Mark]
+    fun: Fun
+    parameters: Parameters
+    results: Results
+    marks: Marks
     id: str  # noqa: A003
 
 
-CASE_PREFIX = "case"
-CASE = f"{CASE_PREFIX}_"
+def get_nb_namespace(
+    nb: str, parameters: Parameters = NO_PARAMS, results: Results = NO_RESULTS
+) -> dict[str, Any]:
+    """Get notebook namespace, optionally parametrizing it."""
+    nb_client = PloomberClient(reads(nb, as_version=NO_CONVERT))
+    # We can't just `nb_client.execute(parameters=...)` since`nb_client.get_namespace()`
+    # would execute all over again
+    if parameters:
+        parametrize_notebook(nb_client._nb, parameters=parameters)  # noqa: SLF001
+    namespace = nb_client.get_namespace()
+    return {result: namespace[result] for result in results} if results else namespace
 
 
-def walk_module_cases(cases: ModuleType) -> Iterator[Case]:
+def hash_get_nb_namespace_args(args, _kwds) -> str:
+    """Hash function for cachier that freezes mappings."""
+    nb: str
+    parameters: Parameters = NO_PARAMS
+    results: Results = NO_RESULTS
+    nb, parameters, results = args
+    return _default_hash_func((nb, frozenset(parameters.items()), tuple(results)), {})
+
+
+@cachier(hash_func=hash_get_nb_namespace_args)
+def get_cached_nb_namespace(
+    nb: str, parameters: Parameters = NO_PARAMS, results: Results = NO_RESULTS
+) -> SimpleNamespace:
+    """Get parametrized namespace."""
+    ns_map = get_nb_namespace(nb, parameters=parameters)
+    return SimpleNamespace(**{result: ns_map[result] for result in results})
+
+
+def get_notebook_cases(
+    notebook: Path,
+    cases: ModuleType,
+    parameters: Parameters = NO_PARAMS,
+    results: Results = NO_RESULTS,
+    marks: Marks = NO_MARKS,
+) -> Iterator[NotebookCase]:
+    """Get cases."""
+    yield from (  # type: ignore
+        pytest.param((notebook, fun, parameters, results), marks=marks, id=id)
+        for fun, parameters, results, marks, id in walk_module_cases(  # noqa: A001
+            cases, parameters, results, marks
+        )
+    )
+
+
+def walk_module_cases(
+    cases: ModuleType,
+    parameters: Parameters = NO_PARAMS,
+    results: Results = NO_RESULTS,
+    marks: Marks = NO_MARKS,
+) -> Iterator[Case]:
     """Walk a case module."""
-    for case in (c.removeprefix(CASE) for c in dir(cases) if c.startswith(CASE)):
-        if isclass(cls := getattr(cases, f"{CASE}{case}")):
-            yield from walk_class_cases(cls)
-        else:
-            yield Case(fun=cls, parameters={}, marks=[], id=case)
-    for casemap in (
-        attr
-        for name, attr in getmembers(cases)
-        if isinstance(attr, dict) and name == f"{CASE_PREFIX}s"
-    ):
-        for fun, all_parameters in casemap.items():
-            name = fun.__name__
-            for i, parameters in enumerate(all_parameters):
-                yield Case(fun=fun, parameters=parameters, marks=[], id=f"{name}_{i}")
+    members = {tuple(name.split("_")): attr for name, attr in getmembers(cases)}
+    # First, bind module-level parameters, results, and marks
+    for name, attr in members.items():
+        match name:
+            case ["parameters"]:
+                parameters = attr
+            case ["results"]:
+                results = attr
+            case ["marks"]:
+                marks = attr
+            case _:
+                pass
+    # Then, walk the rest of the module
+    for name, attr in members.items():
+        match name:
+            case ["cases"]:
+                yield from walk_casemap(attr, parameters, results, marks)
+            case ["case", *case] if isclass(attr):
+                yield from walk_class_cases(attr, parameters, results, marks)
+            case ["case", *case] if not isclass(attr):
+                yield Case(attr, parameters, results, marks, id="_".join(case))
+            case _:
+                pass
 
 
-def walk_class_cases(cls: type) -> Iterator[Case]:
+def walk_casemap(
+    casemap: dict[Fun, list[dict[Literal["parameters", "results", "marks"], Any]]],
+    parameters: Parameters = NO_PARAMS,
+    results: Results = NO_RESULTS,
+    marks: Marks = NO_MARKS,
+) -> Iterator[Case]:
+    yield from (
+        Case(
+            fun,
+            cases.get("parameters", parameters),
+            cases.get("results", results),
+            cases.get("marks", marks),
+            id=f"{fun.__name__}_{i}",
+        )
+        for fun, all_cases in casemap.items()
+        for i, cases in enumerate(all_cases)
+    )
+
+
+def walk_class_cases(
+    cls: type,
+    parameters: Parameters = NO_PARAMS,
+    results: Results = NO_RESULTS,
+    marks: Marks = NO_MARKS,
+) -> Iterator[Case]:
     """Walk a case class."""
-    case = cls.__name__.removeprefix(CASE)
-    first_case = getmembers(cls, isfunction)[0][0]
-    parameters = cls.parameters  # type: ignore
-    marks = getattr(cls, "marks", [])
-    all_parameters = (
-        parameters
-        # Basic check for dict of parameters rather than bare parameters dict
-        if all(isinstance(p, dict) for p in parameters.values())
-        else {first_case: parameters}
-    )
-    all_marks = (
-        marks
-        # Basic check for dict of markers rather than markers list
-        if isinstance(marks, dict)
-        else {first_case: marks}
-    )
+    case = cls.__name__.removeprefix("case_")
     for subcase, fun in getmembers(cls, isfunction):
-        parameters = all_parameters[subcase]
-        marks = all_marks[subcase]
         subcase = subcase.strip("_")
         yield Case(
-            fun, parameters, marks, id="_".join([case, subcase]) if subcase else case
+            fun,
+            getattr(cls, "parameters", parameters),
+            getattr(cls, "results", results),
+            getattr(cls, "marks", marks),
+            id="_".join([case, subcase]) if subcase else case,
         )
